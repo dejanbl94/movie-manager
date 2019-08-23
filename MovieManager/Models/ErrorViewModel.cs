fd26794faa43f675db23730d732ea5421eb0f958
Dejan Tamamovic <dejanbltamamovic@gmail.com>
2019-08-23 13:47:34 +0200

    Add project files.

---
+++ MovieManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
